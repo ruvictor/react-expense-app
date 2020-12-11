a6c72ad59890d06db2ed4528057ed3b85f0b8a44
Victor <rusuvictor30@gmail.com>
2020-12-10 21:11:29 -0500

    Login in process

--- src/components/Forms/Login.js
+++ src/components/Forms/Login.js
@@ -1,15 +1,55 @@
+import React, {Component} from 'react';
+import fire from '../../config/Fire';
 import './Login.css';
 
-const Login = () => {
-    return (
-        <>
-            <form>
-                <input type="text" className="regField" placeholder="email" />
-                <input type="text" className="regField" placeholder="pasword" />
-                <input type="submit" className="submitBtn" value="LOGIN" />
-            </form>
-        </>
-    );
-}
+class Login extends Component {
+    state = {
+        email: '',
+        password: '',
+        fireErrors: ''
+    }
 
+    login = e => {
+        e.preventDefault();
+        fire.auth().signInWithEmailAndPassword(this.state.email, this.state.password)
+        .catch((error) => {
+            this.setState({fireErrors: error.message})
+        });
+    }
+
+    handleChange = e => {
+        this.setState({[e.target.name]: e.target.value});
+    }
+
+    render() {
+
+        let errorNotification = this.state.fireErrors ? 
+            ( <div className="Error"> {this.state.fireErrors} </div> ) : null;
+
+        
+        return (
+            <>
+                {errorNotification}
+                <form>
+                    <input type="text"
+                        className="regField"
+                        placeholder="email"
+                        value={this.state.email} 
+                        onChange={this.handleChange}
+                        name="email"
+                        />
+                    <input
+                        className="regField"
+                        placeholder="pasword"
+                        value={this.state.password} 
+                        onChange={this.handleChange}
+                        name="password"
+                        type="password"
+                    />
+                    <input className="submitBtn" type="submit" onClick={this.login} value="ENTER" />
+                </form>
+            </>
+        );
+    }
+}
 export default Login;
