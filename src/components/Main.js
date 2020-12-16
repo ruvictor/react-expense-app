887f3397c695e4c4103940b56e42177afe803ead
Victor <rusuvictor30@gmail.com>
2020-12-15 23:20:14 -0500

    Last changes

--- src/components/Main.js
+++ src/components/Main.js
@@ -47,9 +47,16 @@ export default class Main extends Component {
                 {!this.state.user ? (
                     <div className="mainBlock">
                       {form}
-                      <span className="underLine">Not registered? <button onClick={() => this.formSwitcher(!this.state.formSwitcher ? 'register' : 'login')} 
-                        className="linkBtn">Create an account</button>
-                      </span>
+                      {
+                        !this.state.formSwitcher ?
+                        (<span className="underLine">Not registered? <button onClick={() => this.formSwitcher(!this.state.formSwitcher ? 'register' : 'login')} 
+                          className="linkBtn">Create an account</button>
+                        </span>) : (
+                          <span className="underLine">Have an account already? <button onClick={() => this.formSwitcher(!this.state.formSwitcher ? 'register' : 'login')} 
+                          className="linkBtn">Sign in here</button>
+                        </span>
+                        )
+                      }
                     </div>
                   ) : (<Tracker />)}
             </>
